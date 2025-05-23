6a0a37ce9faa30fbf6ef57dba7250f1591561ed5
dk <matison@gmail.com>
2025-05-23 20:01:07 +0300

    fix: update questions keyboard logic

--- src/keyboards/questions.py
+++ src/keyboards/questions.py
@@ -2,7 +2,7 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from src.texts.messages import QUESTION_DELETE, QUESTION_LOAD_MORE
 
-ANSWER_VALUES = [(-2, "👎👎"), (-1, "👎"), (0, "⏭️"), (1, "👍"), (2, "👍👍")]
+ANSWER_VALUES = [(-2, "👎👎"), (-1, "👎"), (0, "⏩"), (1, "👍"), (2, "👍👍")]
 ANSWER_VALUE_TO_EMOJI = dict(ANSWER_VALUES)
 
 def get_answer_keyboard(question_id, is_author=False, is_creator=False):
