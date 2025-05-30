698b06288d00315e1d86bcd438b4bdc3f1ee4ab4
dk <matison@gmail.com>
2025-05-30 16:09:10 +0300

    Clean Alembic migration chain, fix cyclic FKs, production-ready schema

--- alembic/env.py
+++ alembic/env.py
@@ -4,12 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 from src.models import Base
-from src.config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, POSTGRES_PORT
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -77,12 +72,6 @@ def run_migrations_online() -> None:
         with context.begin_transaction():
             context.run_migrations()
 
-
-def get_url():
-    return f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
-
-# Устанавливаем строку подключения из переменных окружения
-config.set_main_option("sqlalchemy.url", get_url())
 
 if context.is_offline_mode():
     run_migrations_offline()
